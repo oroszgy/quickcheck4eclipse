b52e4d456fb6b1e2c9fae36984a4da67e396e9e5
Orosz György <G.Orosz@kent.ac.uk>
2010-04-20 16:35:14 +0200

    Properties menu structure is added.

---
+++ org.quickcheck.integration/src/org/quickcheck/integration/handlers/PropertiesHandler.java
@@ -0,0 +1,31 @@
+package org.quickcheck.integration.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class PropertiesHandler extends AbstractHandler {
+
+	/**
+	 * The constructor.
+	 */
+	public PropertiesHandler() {
+
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		String s = event.getCommand().getId();
+		IWorkbenchWindow window = HandlerUtil
+				.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(window.getShell(), s,
+				"Hello, Eclipse world");
+		return null;
+	}
+}
